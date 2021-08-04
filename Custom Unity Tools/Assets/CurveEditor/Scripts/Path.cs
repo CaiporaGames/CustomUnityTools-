3623a1dfe7fbac5472530e89aca47ad9587fdbbe
Caipora Games <73959333+CaiporaGames@users.noreply.github.com>
2021-08-04 18:36:48 -0300

    Curve editor
    I am implementing a curve editor.

---
+++ Custom Unity Tools/Assets/CurveEditor/Scripts/Path.cs
@@ -0,0 +1,229 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Path
+{
+    [SerializeField, HideInInspector]
+    List<Vector2> points;
+    [SerializeField, HideInInspector]
+    bool isClosed;
+    [SerializeField, HideInInspector]
+    bool autoSetControlPoints;
+
+    public Path(Vector2 center)// Given two points. Center is the center between them
+    {
+        points = new List<Vector2>()
+        {
+            center + Vector2.left,
+            center + (Vector2.left + Vector2.up) * 0.5f,
+            center + (Vector2.right + Vector2.down)*0.5f,
+            center + Vector2.right
+        };
+    }
+
+    public Vector2 this[int i]
+    {
+        get
+        {
+            return points[i];
+        }
+    }
+
+    public bool AutoSetControlPoints
+    {
+        get
+        {
+            return autoSetControlPoints;
+        }
+        set
+        {
+            if (autoSetControlPoints != value)
+            {
+                autoSetControlPoints = value;
+
+                if (autoSetControlPoints)
+                {
+                    AutoSetAllControlPoits();
+                }
+            }
+        }
+    }
+
+    public int NumPoints
+    {
+        get
+        {
+            return points.Count;
+        }
+    }
+
+    public int NumSegments
+    {
+        get
+        {
+            return points.Count / 3;
+        }
+    }
+
+    
+    public void AddSegment(Vector2 anchorPos)//anchor position is the position of the new created control point.
+    {
+        points.Add(points[points.Count - 1] * 2 - points[points.Count - 2]);
+        points.Add((points[points.Count - 1] + anchorPos) * 0.5f);
+        points.Add(anchorPos);
+
+        if (autoSetControlPoints)
+        {
+            AutoSetAllAffectedControlPoints(points.Count - 1);
+        }
+    }
+
+    public Vector2[] GetPointsInSegments(int i)
+    {
+        return new Vector2[]
+        {
+            points[i * 3],
+            points[i * 3 + 1],
+            points[i * 3 + 2],
+            points[LoopIndex(i * 3 + 3)]
+        };
+    }
+
+    public void MovePoint(int i, Vector2 position)
+    {
+        Vector2 deltaMove = position - points[i];
+
+        if (i % 3 == 0 || !autoSetControlPoints)
+        {
+            points[i] = position;
+
+            if (autoSetControlPoints)
+            {
+                AutoSetAllAffectedControlPoints(i);
+            }
+            else
+            {
+                if (i % 3 == 0)
+                {
+                    if (i + 1 < points.Count || isClosed)
+                    {
+                        points[LoopIndex(i + 1)] += deltaMove;
+                    }
+                    if (i - 1 >= 0 || isClosed)
+                    {
+                        points[LoopIndex(i - 1)] += deltaMove;
+                    }
+                }
+                else
+                {
+                    bool nextPointIsAnchor = (i + 1) % 3 == 0;
+                    int correspondingControlIndex = nextPointIsAnchor ? i + 2 : i - 2;
+                    int anchorIndex = nextPointIsAnchor ? i + 1 : i - 1;
+
+                    if (correspondingControlIndex >= 0 && correspondingControlIndex < points.Count || isClosed)
+                    {
+                        float distance = (points[LoopIndex(anchorIndex)] - points[LoopIndex(correspondingControlIndex)]).magnitude;
+                        Vector2 direction = (points[LoopIndex(anchorIndex)] - position).normalized;
+                        points[LoopIndex(correspondingControlIndex)] = points[LoopIndex(anchorIndex)] + direction * distance;
+                    }
+                }
+            }
+        }
+    }
+
+    public void ToggleClose()
+    {
+        isClosed = !isClosed;
+
+        if (isClosed)
+        {
+            points.Add(points[points.Count - 1] * 2 - points[points.Count - 2]);
+            points.Add(points[0] * 2 - points[1]);
+
+            if (autoSetControlPoints)
+            {
+                AutoSetAnchorControlPoints(0);
+                AutoSetAnchorControlPoints(points.Count - 3);
+            }
+        }
+        else
+        {
+            points.RemoveRange(points.Count - 2, 2);
+
+            if (autoSetControlPoints)
+            {
+                AutoSetStartAndEndControls();
+            }
+        }
+    }
+
+    int LoopIndex(int i)
+    {
+        return (i + points.Count) % points.Count;
+    }
+
+    void AutoSetAllAffectedControlPoints(int updatedAnchorIndex)
+    {
+        for (int i = updatedAnchorIndex - 3; i <= updatedAnchorIndex + 3; i+= 3)
+        {
+            if (i >= 0 && i < points.Count || isClosed)
+            {
+                AutoSetAnchorControlPoints(LoopIndex(i));
+            }
+        }
+
+        AutoSetStartAndEndControls();
+    }
+
+    public void AutoSetAllControlPoits()
+    {
+        for (int i = 0; i < points.Count; i += 3)
+        {
+            AutoSetAnchorControlPoints(i);
+        }
+
+        AutoSetStartAndEndControls();
+    }
+
+    void AutoSetStartAndEndControls()
+    {
+        if (!isClosed)
+        {
+            points[1] = (points[0] + points[2]) * 0.5f;
+            points[points.Count - 2] = (points[points.Count - 1] + points[points.Count - 3]) * 0.5f;
+        }
+    }
+
+    void AutoSetAnchorControlPoints(int anchorIndex)
+    {
+        Vector2 anchorPosition = points[anchorIndex];
+        Vector2 direction = Vector2.zero;
+        float[] neighbourDistance = new float[2];
+
+        if (anchorIndex - 3 >= 0 || isClosed)
+        {
+            Vector2 offset = points[LoopIndex(anchorIndex - 3)] - anchorPosition;
+            direction += offset.normalized;
+            neighbourDistance[0] = offset.magnitude;
+        }
+        if (anchorIndex + 3 >= 0 || isClosed)
+        {
+            Vector2 offset = points[LoopIndex(anchorIndex + 3)] - anchorPosition;
+            direction -= offset.normalized;
+            neighbourDistance[1] = -offset.magnitude;
+        }
+            
+        direction.Normalize();
+
+        for (int i = 0; i < 2; i++)
+        {
+            int controlIndex = anchorIndex + i * 2 - 1;
+            if (controlIndex >= 0 && controlIndex < points.Count || isClosed)
+            {
+                points[LoopIndex(controlIndex)] = anchorPosition + direction * neighbourDistance[i] * 0.5f;
+            }
+        }
+    }
+}
---
+++ Custom Unity Tools/Assets/Editor/PathEditor.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PathCreator))]
+public class PathEditor : Editor
+{
+    PathCreator creator;
+    Path path;
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        EditorGUI.BeginChangeCheck();
+
+        if (GUILayout.Button("Create new"))
+        {
+            Undo.RecordObject(creator, "Create new");
+
+            creator.CreatePath();
+            path = creator.path;
+        }
+
+        if (GUILayout.Button("Toggle closed"))
+        {
+            Undo.RecordObject(creator, "Toggle clossed");
+
+            path.ToggleClose();
+        }
+
+        bool autoSetControlPoints = GUILayout.Toggle(path.AutoSetControlPoints, "Auto Set Control Points");
+
+        if (autoSetControlPoints != path.AutoSetControlPoints)
+        {
+            Undo.RecordObject(creator, "Toggle auto set controls");
+
+            path.AutoSetControlPoints = autoSetControlPoints;
+        }
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            SceneView.RepaintAll();
+        }
+    }
+
+    private void OnSceneGUI()
+    {
+        Input();
+        Draw();
+    }
+
+    void Input()
+    {
+        Event guiEvent = Event.current;
+
+        Vector2 mousePosition = HandleUtility.GUIPointToWorldRay(guiEvent.mousePosition).origin;
+
+        if (guiEvent.type == EventType.MouseDown && guiEvent.button == 0 && guiEvent.shift)
+        {
+            Undo.RecordObject(creator, "Add segment");
+            path.AddSegment(mousePosition);
+        }
+    }
+
+    void Draw()
+    {
+        for (int i = 0; i < path.NumSegments; i++)
+        {
+            Vector2[] points = path.GetPointsInSegments(i);
+           
+            Handles.color = Color.black;
+            Handles.DrawLine(points[1], points[0]);
+            Handles.DrawLine(points[2], points[3]);
+            Handles.DrawBezier(points[0],points[3], points[1], points[2],Color.green, null, 2);
+        }
+        Handles.color = Color.red;
+
+        for (int i = 0; i < path.NumPoints; i++)
+        {
+           Vector2 newPosition = Handles.FreeMoveHandle(path[i], Quaternion.identity, 0.1f, Vector2.zero, Handles.CylinderHandleCap);
+
+            if (path[i] != newPosition)
+            {
+                Undo.RecordObject(creator, "Move point");
+                path.MovePoint(i, newPosition);
+            }
+        }
+    }
+
+    private void OnEnable()
+    {
+        creator = (PathCreator)target;
+
+        if (creator.path == null)
+        {
+            creator.CreatePath();
+        }
+
+        path = creator.path;
+    }
+}
